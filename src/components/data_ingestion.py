237a0174b14d790e75dbccf069021f8151bcd2f7
Dibya069 <dibyamohanty069@gmail.com>
2023-05-18 19:27:53 +0530

    05_DataIngestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,7 +24,24 @@ class DataIngestion:
         logging.info("Data Ingestion Method Start")
 
         try:
-            pass
+            df = pd.read_csv(os.path.join('notebooks/data', 'train.csv'))
+            logging.info('Dataset read as a pands Dataframe')
+
+            os.mkdir(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok = True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Raw data is created")
+
+            train_set, test_set = train_test_split(df, test_size=0.3, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
 
         except Exception as e:
             logging.info('Exception Occured in DataIngestion Stage')
