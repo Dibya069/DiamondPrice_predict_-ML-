8aa73103b97b9ce2341f492161d0aaab51d0e175
Dibya069 <dibyamohanty069@gmail.com>
2023-05-22 20:46:18 +0530

    06.Data_Transformation(1)

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,19 @@
-## It has all the common functionality that are required 
+import sys
+import os
+import pickle
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_obj(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
